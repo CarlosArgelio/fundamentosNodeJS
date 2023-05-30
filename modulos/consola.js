13d8c6671d1122a9c394357fc1189f5dfdd0c5ca
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-05-30 08:02:04 -0400

    console js

---
+++ modulos/consola.js
@@ -0,0 +1,25 @@
+// console.error('Error')
+// // Muestra los errores de forma visual
+
+// console.warn('Advertencia')
+// // Muestra las advertencias de forma visual
+
+// var address = {
+//     'street': 'Calle 1',
+//     'number': '1',
+//     'departament': 'B32'
+// }
+
+// var table = [
+//     {
+//         'name': 'Carlos',
+//         'age': 24,
+//         'address': address
+//     },
+//     {
+//         'name': 'Argelio',
+//         'age': 25,
+//         'address': address
+//     }
+// ]
+// console.table(table)
