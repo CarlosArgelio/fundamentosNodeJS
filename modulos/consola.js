53cafb4191b176690b154565c1dbee0c60216f0b
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-06-02 10:03:09 -0400

    proceso hijo js

--- modulos/consola.js
+++ modulos/consola.js
@@ -1,25 +1,25 @@
-// console.error('Error')
-// // Muestra los errores de forma visual
+console.error('Error')
+// Muestra los errores de forma visual
 
-// console.warn('Advertencia')
-// // Muestra las advertencias de forma visual
+console.warn('Advertencia')
+// Muestra las advertencias de forma visual
 
-// var address = {
-//     'street': 'Calle 1',
-//     'number': '1',
-//     'departament': 'B32'
-// }
+var address = {
+    'street': 'Calle 1',
+    'number': '1',
+    'departament': 'B32'
+}
 
-// var table = [
-//     {
-//         'name': 'Carlos',
-//         'age': 24,
-//         'address': address
-//     },
-//     {
-//         'name': 'Argelio',
-//         'age': 25,
-//         'address': address
-//     }
-// ]
-// console.table(table)
+var table = [
+    {
+        'name': 'Carlos',
+        'age': 24,
+        'address': address
+    },
+    {
+        'name': 'Argelio',
+        'age': 25,
+        'address': address
+    }
+]
+console.table(table)
